490897ac562bfb9811bb995052d641979b71c18c
Chell <im.chell@outlook.com>
2022-04-16 17:49:29 +0800

    feat: detect scroll position

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import getScrollPercentage from "./scroll";
+
+/**
+ * Detect shortcut to add position mark
+ */
 document.addEventListener("keydown", function shortcutDetected(event) {
   if (event.ctrlKey && event.key == "b") {
     console.log("shortcut detected");
+    console.log(getScrollPercentage());
   }
 });
---
+++ src/scroll.js
@@ -0,0 +1,48 @@
+/**
+ * Get current browser viewpane heigtht
+ */
+function _get_window_height() {
+  return (
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight ||
+    0
+  );
+}
+
+/**
+ * Get current absolute window scroll position
+ */
+function _get_window_Yscroll() {
+  return (
+    window.pageYOffset ||
+    document.body.scrollTop ||
+    document.documentElement.scrollTop ||
+    0
+  );
+}
+
+/**
+ * Get current absolute document height
+ */
+function _get_doc_height() {
+  return Math.max(
+    document.body.scrollHeight || 0,
+    document.documentElement.scrollHeight || 0,
+    document.body.offsetHeight || 0,
+    document.documentElement.offsetHeight || 0,
+    document.body.clientHeight || 0,
+    document.documentElement.clientHeight || 0
+  );
+}
+
+/**
+ * Get current vertical scroll percentage
+ */
+function getScrollPercentage() {
+  return (
+    ((_get_window_Yscroll() + _get_window_height()) / _get_doc_height()) * 100
+  );
+}
+
+export default getScrollPercentage;
