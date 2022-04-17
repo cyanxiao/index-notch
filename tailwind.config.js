748156d09e24af48eae91ccd421c84877ed84fc4
Chell <im.chell@outlook.com>
2022-04-17 17:29:13 +0800

    update: style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./popup/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./popup/index.html"],
   theme: {
-    extend: {},
+    colors: {
+      yellow: "#F3CC88",
+      red: "#C04754",
+      black: "#313B52",
+      white: "#F3F1E4",
+      blue: "#313B52",
+      lightred: "#CD6D78",
+      lightblue: "#4E5464",
+      darkred: "#9D2734",
+      darkblue: "#1C222F",
+    },
+    fontFamily: {
+      sans: ["Patrick Hand", "sans-serif"],
+    },
   },
-  plugins: [],
 };
