b456cd72ff71928aef26a0dfd8f9a704b8e59fa7
Chell <im.chell@outlook.com>
2022-04-17 10:06:31 +0800

    fix: add content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./content/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
